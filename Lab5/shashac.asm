b7bf50c3900766d2e347ac480351324299a5233e
X1_LAPTOP\seekc <seekchange@daum.net>
2023-05-30 14:41:12 +0900

    lab5 네번째

--- Lab5/shashac.asm
+++ Lab5/shashac.asm
@@ -1,22 +1,22 @@
 buffer=$C000
 out=$2300
-count=$10
+accumVal=$11
+
     ldx #$FF
     txs
 
     lda buffer
     asl
     asl
-    clc
-    sta count
-
-round:
-    ldx #0
+    sec
+    sbc #4
+    tax
 
 first:
-    lda buffer+2  ; 바로빠져나가는 경우:2+32+9 43사이클후 체크
-    sta out       ; 두번도는경우 : 2+20+9 (39) + 51+9(60) = 99
-  
+
+    lda buffer+2
+    sta out
+
     lda buffer+3
     sta out+1
  
@@ -26,35 +26,68 @@ first:
     lda buffer+5
     sta out+3
 
-    ldx #4
-    cpx count
-    bcs check
+    txa
+    beq keep
 
-accum:      ; 0x802B
+accum:
 
-    lda out
-    eor buffer+2,x
-    sta out
+    lda out+3
+    eor buffer+5,x
+    sta out+3
+    
+    lda out+2
+    eor buffer+4,x
+    sta out+2
 
     lda out+1
     eor buffer+3,x
     sta out+1
 
+    lda out
+    eor buffer+2,x
+    sta out
+
+    txa
+    sbc #4
+    tax
+    
+    bne accum
+    
+keep:
+
+    lda out
+    sta accumVal
+    
+    lda out+1
+    sta accumVal+1
+    
     lda out+2
-    eor buffer+4,x
+    sta accumVal+2
+ 
+    lda out+3
+    sta accumVal+3
+
+    jmp check
+
+loop:   ; 0x8058
+
+    lda out
+    eor accumVal
+    sta out
+
+    lda out+1
+    eor accumVal+1
+    sta out+1
+
+    lda out+2
+    eor accumVal+2
     sta out+2
 
     lda out+3
-    eor buffer+5,x
+    eor accumVal+3
     sta out+3
 
-    txa       ; 0x804F
-    adc #4
-    cmp count
-    tax
-    bcc accum
-
-check:            ; 0x8057 c가 1 상태임
+check:            ; 0x806c c가 1 상태임
 
     lda buffer+1
     bit out+3
@@ -62,14 +95,14 @@ check:            ; 0x8057 c가 1 상태임
     
     bit out+2
     beq continue
-  
+
     bit out+1
     beq continue
 
     bit out
     bne end
 
-continue:        ; 여전히 c는 1
+continue:        ; 0x8083 여전히 c는 1
     
     lda out
     asl
@@ -91,7 +124,6 @@ continue:        ; 여전히 c는 1
     ora #1
     sta out+3
 
-    ldx #0
-    jmp accum
+    jmp loop    ; 0x80A7
 
 end:
